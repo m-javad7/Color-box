e9d28f65bd4a762a28ea0b8c311d017e6e2b9864
javad7721 <m.javad7721@gmail.com>
2023-06-21 17:57:44 +0330

    add Components:add ColorBox

---
+++ src/Components/ColorBox.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+const ColorBox = ({ backgroundColor }) => {
+    return (
+        <div className="color-box" style={{backgroundColor}}/>
+    );
+};
+
+export default ColorBox;
